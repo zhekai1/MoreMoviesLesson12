070cecd0be9ca904c768f6ff6190a9a27f09793b
Kai <21024750@myrp.edu.sg>
2022-08-04 21:56:44 +0800

    cnp

--- app/src/main/java/sg/edu/rp/c346/id21024750/mymovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21024750/mymovies/MainActivity.java
@@ -1,8 +1,12 @@
 package sg.edu.rp.c346.id21024750.mymovies;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnInsert;
     Button btnShowList;
     Button btnShowMovies;
+    Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +46,30 @@ public class MainActivity extends AppCompatActivity {
         btnInsert = findViewById(R.id.btnInsert);
         btnShowList = findViewById(R.id.btnShowList);
         btnShowMovies = findViewById(R.id.btnShowMovies);
+        btnUpdate = findViewById(R.id.btnUpdate);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to delete the movie");
+                myBuilder.setCancelable(false);
+
+                myBuilder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        .setText("You have selected Positive.");
+
+                    }
+                });
+
+                myBuilder.setNeutralButton("Cancel", null);
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+
+            }
+        });
+
     }
 }
